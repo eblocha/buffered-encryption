dc7267932eb74cf9bc48b8fb775a1b0c1329a97d
Elliott Blocha <eblocha@gmail.com>
2021-02-27 12:19:10 -0500

    formatting

--- buffered_encryption/crypt.py
+++ buffered_encryption/crypt.py
@@ -18,7 +18,7 @@ class EncryptionIterator:
         Additional data used to verify the key later
     chunk_size : int
         How much data to encrypt per iteration
-    
+
     Attributes
     ----------
     iv : bytes
@@ -26,25 +26,30 @@ class EncryptionIterator:
     tag : bytes
         The tag to verfiy data integrity on decryption
     """
-    def __init__(self,plaintext:io.BytesIO,key:bytes,signature:bytes,chunk_size:int=64 * 1024):
+
+    def __init__(
+        self,
+        plaintext: io.BytesIO,
+        key: bytes,
+        signature: bytes,
+        chunk_size: int = 64 * 1024,
+    ):
         self.iv = os.urandom(12)
         self.file = plaintext
         self.chunk_size = chunk_size
-        self.encryptor = Cipher(
-            algorithms.AES(key),
-            modes.GCM(self.iv)
-        ).encryptor()
+        self.encryptor = Cipher(algorithms.AES(key), modes.GCM(self.iv)).encryptor()
         self.encryptor.authenticate_additional_data(signature)
-    
+
     @property
     def tag(self):
         return self.encryptor.tag
-    
+
     def __iter__(self):
         for chunk in iter_chunks(self.file, chunk_size=self.chunk_size):
             yield self.encryptor.update(chunk)
-        
+
         yield self.encryptor.finalize()
+
 
 class DecryptionIterator:
     """
@@ -65,19 +70,27 @@ class DecryptionIterator:
     chunk_size : int
         How much data to decrypt per iteration
     """
-    def __init__(self,ciphertext:io.BytesIO,key:bytes,signature:bytes,iv:bytes,tag:bytes,chunk_size:int=64 * 1024):
+
+    def __init__(
+        self,
+        ciphertext: io.BytesIO,
+        key: bytes,
+        signature: bytes,
+        iv: bytes,
+        tag: bytes,
+        chunk_size: int = 64 * 1024,
+    ):
         self.iv = iv
         self.tag = tag
         self.file = ciphertext
         self.chunk_size = chunk_size
         self.decryptor = Cipher(
-            algorithms.AES(key),
-            modes.GCM(self.iv,self.tag)
+            algorithms.AES(key), modes.GCM(self.iv, self.tag)
         ).decryptor()
         self.decryptor.authenticate_additional_data(signature)
-    
+
     def __iter__(self):
         for chunk in iter_chunks(self.file, chunk_size=self.chunk_size):
             yield self.decryptor.update(chunk)
-        
+
         yield self.decryptor.finalize()
