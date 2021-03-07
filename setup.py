5ee84645f90ab74d2a7f6f7cc826bbe146f3c9cc
Elliott Blocha <eblocha@gmail.com>
2021-03-07 09:54:32 -0500

    bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
 
 setup(
     name = "buffered_encryption",
-    version = "0.2.0",
+    version = "0.2.1",
     author = "Elliott Blocha",
     description = "Encrypt large files without loading the entire file into memory.",
     license = "MIT",
